49bb1825484fd24bca2087fd7ebedd616c09f1e1
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-12-23 13:21:16 +0530

    test while loops

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-price  = 10
-rating = 20.5
-print(price * rating)
+command  = ""
+while True:
+    command = input("> ").lower()
+    if command == "start":
+        print("Car Started...")
+    elif command == "stop":
+        print("Car Stopped...")
+    elif command == "help":
+        print("""
+Start  = To start the car
+Stop   = To stop the car
+Quit   = To exit the game
+        """     )
+    else :
+        print("I don't understand that...")
